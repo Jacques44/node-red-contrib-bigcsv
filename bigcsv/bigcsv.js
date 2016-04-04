595c81903314adaa380159127fc060d0d7eedfd5
Jacques W <jacquesn4444@gmail.com>
2016-04-04 23:30:34 +0200

    Big works and thoughts

--- bigcsv/bigcsv.js
+++ bigcsv/bigcsv.js
@@ -39,160 +39,160 @@ module.exports = function(RED) {
   function BigCSV(config) {
 
     config.checkpoint = 100;
+    config.columns = config.columns || true;
     
     // CSV Parser parameters as described in http://csv.adaltas.com/parse/
-    var csv_options = config;
-
-    // Returns a configured CSV parser based on given configuration (on the fly) or by setup
-    var parser = function(config) {
-      if (!config) config = csv_options;
-
-      // Special case, if no columns specified, first line is a list of columns by default
-      if (! config.columns) config.columns = true;
-
-      return csv.parse(config);
-    }
+    var def_config = config;
 
     RED.nodes.createNode(this, config);
     var node = this;
 
-    var nb = 0;
+    var runtime_control = {
+      config: def_config
+    }
+    delete runtime_control.config.wires;
+    delete runtime_control.config.x;
+    delete runtime_control.config.y;
+    delete runtime_control.config.z;
+
+    // Require stream to close
+    var close_stream = function(input) {
+      if (input) input.end();
+      return;
+    }
+
+    // Principe #2, end message on output #2
+    var on_finish = function(err) {
+
+      runtime_control.state = "end";
+      runtime_control.end = new Date();
+
+      if (err) {
+        runtime_control.state = "error";
+        runtime_control.error = err;
+        node.status({fill: "red", shape: "dot", text: err.message });
+      } else {
+        node.status({fill: "green", shape: "dot", text: "done with " + runtime_control.records + " records" });
+      }
+
+      node.send([undefined, { control: runtime_control }]);
+    }
+
+    var on_start = function(config, control) {
+
+      runtime_control.records = runtime_control.size = 0;
+      runtime_control.start = new Date();
+      runtime_control.control = control;  // parent control message
+      runtime_control.config = config;
+      delete runtime_control.end;
+      runtime_control.state = "start";      
+
+      node.send([undefined, { control: runtime_control }]);    
+    }
+
+    var d;
+
+    // control is an incoming control message { control: {}, config: {} }
+    var create_stream = function(msg) {
+
+      var my_config = (msg || {}).config || def_config;
+      if (! my_config.columns) my_config.columns = true;
+
+      // Error management using domain
+      d = domain.create();
+      d.on('error', function(err) {     
+        on_finish(err);
+        node.error(err);
+      });
+
+
+      var entry;
+
+      // Everything linked together with error management
+      // Cf documentation
+      // Run the supplied function in the context of the domain, implicitly binding all event emitters, timers, and lowlevel requests that are created in that context
+      d.run(function() {   
+
+        // Streams are created in the scope of domain (very very important)
+
+        // 1. Parser
+        var p = csv.parse(my_config);
+
+        // 2. Sender
+        var outstream = new stream.Transform({ objectMode: true });
+        outstream._transform = function(data, encoding, done) {
+
+          // #3 big node principle: tell me what you are doing, so far
+          if (++runtime_control.records % runtime_control.config.checkpoint == 0) node.status({fill: "blue", shape: "dot", text: "sending... " + runtime_control.records + " records so far"});
+
+          // #1 big node principle: send blocks for big files management
+          node.send([{ payload: data }]);
+
+          done();
+        }
+
+        if (my_config.is_filename) {
+
+          (entry = fs.createReadStream(msg.payload || msg.filename, my_config))
+          .pipe(p)
+          .pipe(outstream)
+          .on('finish', on_finish);               
+
+        } else {
+     
+          (entry = p)
+          .pipe(outstream)
+          .on('finish', on_finish);               
+        }
+
+      });
+
+      // Big node status and statistics
+      on_start(my_config, msg.control);
+
+      // Return is the entry point for incoming data
+      return entry;
+    }
 
     // Payload message is a file name?
     if (config.is_filename) {
 
-      // CSV livrary used in pipe mode
-
       // Foreach file name
       this.on('input', function(msg) {
 
-        var options = {}; // pour l'encoding plus tard
-        node.status({fill: "blue", shape: "dot", text: "reading " + msg.payload});
-
-        // Records sent so far
-        nb = 0;
-
-        // Error management using domain
-        var d = domain.create();
-        d.on('error', function(err) {
-          node.status({fill: "red", shape: "dot", text: err.message });
-          node.error(err);
-        });
-
-        var p = parser(msg.options);
-
-        // finish function
-        var finish = function() {
-          p.end();
-          node.status({fill: "green", shape: "dot", text: "done with " + nb + " records"});
-        };
-
-        // Sender
-        var outstream = csv.transform(function(data) {
-          if (++nb % config.checkpoint == 0) node.status({fill: "blue", shape: "dot", text: "reading... " + nb + " so far"});
-          node.send({ payload: data });
-        });        
-
-        // Sender
-        var counter = csv.transform(function(data) {
-          console.log("top");
-
-          return data;
-        });  
-
-        // Go!
-        d.run(function() {        
-          fs.createReadStream(msg.payload, options)
-            .pipe(counter)
-            .pipe(p)
-            .pipe(outstream)
-            .on('finish', finish);               
-        });
+        create_stream(msg);
 
       })
       
     } else {      
 
-      // Used in stream mode
+      var input_stream;
 
-       // Records sent so far
-      var nb;
-
-      var receiver;
-
-      var init = function(options, noready) {
-
-        console.log("New stream");
-
-        // Error management using domain
-        var d = domain.create();
-        d.on('error', function(err) {
-          node.status({fill: "red", shape: "dot", text: err.message });
-          node.error(err);
-        });
-
-        // Sender
-        var outstream = csv.transform(function(data) {
-          if (++nb % config.checkpoint == 0) node.status({fill: "blue", shape: "dot", text: "sending... " + nb + " so far"});
-
-          node.send({ payload: data });
-        });        
-
-        // Simple tunnel for pipes
-        var cat = new stream.Transform({ objectMode: true });
-        cat._transform = function(data, encoding, done) {
-          if (++nb % config.checkpoint == 0) node.status({fill: "blue", shape: "dot", text: "sending... " + nb + " so far"});
-
-            node.send({ payload: data });
-
-          done();
-        };        
-
-        var p = parser(options);
-        
-        // Go!
-        d.run(function() {        
-            p
-            .pipe(outstream)
-            .on('finish', finish);               
-        });
-
-        if (!noready) node.status({fill: "blue", shape: "dot", text: "ready !"});
-
-        nb = 0;
-
-        return p;
-      }
-
-     // finish function
-      var finish = function() {
-        node.status({fill: "green", shape: "dot", text: "done with " + nb + " records"});
-      };      
-
-      receiver = init();
-
-      // If new message...
+      // If any new message...
       this.on('input', function(msg) {
 
-        if (msg.control) {
-          if (msg.control == "start") {
-            // A new start...
-            if (receiver) receiver.end();
-            receiver = init(msg.config);
-            return;
-          }
-          if (msg.control == "end") {
-            receiver.end();
-            receiver = undefined;
-            return;
-          }
+        if (msg.control && msg.control.state == "start") {
+          input_stream = close_stream(input_stream);
+
+          node.status({fill: "blue", shape: "dot", text: "ready !"});
+
+          if (msg.config) input_stream = create_stream(msg);
         }
 
-        if (!receiver) {
-          receiver = init();
+        if (msg.payload) {
+          if (! input_stream) input_stream = create_stream(msg);
+          
+          runtime_control.size += msg.payload.length;
+
+          input_stream.write(msg.payload);
         }
 
-        receiver.write(msg.payload);
+        if (msg.control && msg.control.state == "end") {
+          runtime_control.control = msg.control;    // Parent control message
+
+          input_stream = close_stream(input_stream);
+        }
+       
       })
 
     }
